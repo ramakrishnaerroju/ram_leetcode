bd1b418f6583d4d6b51d96b732a1260fea032d7f
Ramakrishna <38667673+ramakrishnaerroju@users.noreply.github.com>
2020-10-22 07:58:47 +0000

    Sync LeetCode submission - Subarray Sum Equals K (javascript)

---
+++ my-folder/problems/subarray_sum_equals_k/solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    let count=0;
+    let sum=0;
+    let map = new Map();
+    map.set(0,1);
+    for(let i=0;i<nums.length;i++){
+        sum+=nums[i];
+        if(map.has(sum - k))  
+            count+=map.get(sum-k);
+       
+        let times=map.get(sum) || 0;
+        map.set(sum,times+1);      
+        
+       
+    }
+    
+    return count;
+};
